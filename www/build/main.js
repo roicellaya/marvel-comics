6612915536ac6208349f038506d6da225bc4e34f
Roicel Laya <roicel.laya@gmail.com>
2017-05-06 01:14:30 -0300

    Changed marvel endpoint call for test.

--- www/build/main.js
+++ www/build/main.js
@@ -76680,7 +76680,7 @@ var MarvelComicService = (function () {
             // We're using Angular HTTP provider to request the data,
             // then on the response, it'll map the JSON data to a parsed JS object.
             // Next, we process the data and resolve the promise with the new data.
-            _this.http.get('v1/public/comics?apikey=e89a3ff9aba5d84a2ebe8d79c8458948')
+            _this.http.get('https://gateway.marvel.com/v1/public/comics?apikey=e89a3ff9aba5d84a2ebe8d79c8458948')
                 .map(function (res) { return res.json(); })
                 .subscribe(function (data) {
                 // we've got back the raw data, now generate the core schedule data
